d5cfdafc0e69734f6790a2ddb0081fc8c35c4359
Егор <egor_user@MacBook-Air-Egor.local>
2026-08-23 19:47:41 +0400

    chore: версия кэша companion-v2 (сброс SW-кэша после фикса app.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'companion-v1';
+const CACHE = 'companion-v2';
 const SHELL = ['.', 'index.html', 'app.js', 'manifest.webmanifest'];
 self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL))));
 self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))));
